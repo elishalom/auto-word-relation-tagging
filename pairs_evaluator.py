eb57b4d6eacc61635dbdd7b2e912a230eba38d46
elishalom <eli.shalom@gmail.com>
2018-07-21 14:08:07 +0300

    Initial version that allows querying a pair for matching pais

---
+++ pairs_evaluator.py
@@ -0,0 +1,21 @@
+from gensim.models.keyedvectors import Word2VecKeyedVectors
+from scipy.spatial.distance import euclidean, cosine
+
+
+class PairsEvaluator(object):
+    __DISTANCE_THRESHOLD = 0.5
+
+    def __init__(self, model: Word2VecKeyedVectors, source_word: str, target_word: str) -> None:
+        super().__init__()
+        self.__model = model
+        self.__diff_vector = model.wv.get_vector(target_word) - model.wv.get_vector(source_word)
+
+    def distance(self, source_word, target_word):
+        wv = self.__model.wv
+        pair_diff_vector = wv.get_vector(target_word) - wv.get_vector(source_word)
+        return euclidean(self.__diff_vector, pair_diff_vector)
+
+    def are_similar(self, source_word, target_word):
+        wv = self.__model.wv
+        pair_diff_vector = wv.get_vector(target_word) - wv.get_vector(source_word)
+        return cosine(self.__diff_vector, pair_diff_vector) < self.__DISTANCE_THRESHOLD
